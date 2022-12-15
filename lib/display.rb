d337432f27ee6dccf87792430021313c719249d7
Matt <msb@ameritech.net>
2022-12-15 08:12:30 -0500

    Create chess piece unicode file, create rough board

--- lib/display.rb
+++ lib/display.rb
@@ -8,25 +8,21 @@ class Display
   def print_temp_board
     system 'cls'
     puts ""
-    black_square = :black       # TEMP, in future use square's color
+    black_square = :light_black       # TEMP, in future use square's color
     white_square = :white       # TEMP, in future use square's color
 
     4.times do 
-      3.times do
-        4.times do
-          print "        ".colorize(:color => :black, :background => white_square)
-          print "        ".colorize(:color => :black, :background => black_square)
-        end
-        print "\n"
+      4.times do
+        print "    ".colorize(:color => :black, :background => white_square)
+        print "    ".colorize(:color => :black, :background => black_square)
       end
+      print "\n"
 
-      3.times do
-        4.times do
-          print "        ".colorize(:color => :black, :background => black_square)
-          print "        ".colorize(:color => :black, :background => white_square)
-        end
-        print "\n"
+      4.times do
+        print "    ".colorize(:color => :black, :background => black_square)
+        print "    ".colorize(:color => :black, :background => white_square)
       end
+      print "\n"
     end
 
     puts ""
